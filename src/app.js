eb6bdd24eb767fe938b9d64f18a57f1ae879b8b3
Jonas Berx <43341402+JonasBerx@users.noreply.github.com>
2026-05-10 15:32:08 +0200

    feat: serve Vite build from Express, SPA fallback to index.html

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const createRsvpRouter = require('./routes/rsvp');
 const createAdminRouter = require('./routes/admin');
 
@@ -11,6 +12,13 @@ function createApp(db) {
   }));
   app.use('/api/rsvp', createRsvpRouter(db));
   app.use('/admin', createAdminRouter(db));
+
+  const distDir = path.join(__dirname, '../dist');
+  app.use(express.static(distDir));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(distDir, 'index.html'));
+  });
+
   return app;
 }
 
